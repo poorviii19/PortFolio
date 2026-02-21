cee0d9d62fd78cffd054e42df1b9a8bf5321de95
poorviii19 <poorvishukla2005@gmail.com>
2026-02-21 13:43:33 +0530

    Remove Live Button(not deployed projects)

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,7 @@ fetch("data/projects.json")
             <img src="${p.image}" class="img-fluid mb-3 rounded">
             <h5>${p.title}</h5>
             <p>${p.description}</p>
-            <a href="${p.demo}" class="btn btn-primary btn-sm">Live</a>
-            <a href="${p.github}" class="btn btn-outline-light btn-sm">Code</a>
+            <a href="${p.github}" class="btn btn-primary btn-sm">Github</a>
           </div>
         </div>
       `;
